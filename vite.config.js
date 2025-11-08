af8b52a1f2f021b5a2d61461eb5859431dd8e20d
I Putu Sukma Widyantara <68043141+sukmaputu@users.noreply.github.com>
2025-11-08 07:46:27 -0800

    perbaikan

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/diro_technical_test/',
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: 'classic' 
+    })
+  ],
 });
